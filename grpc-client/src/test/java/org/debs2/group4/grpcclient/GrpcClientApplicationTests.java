3d8f96aa546126d3b863258a98fae7045afa2606
Jinseong Hwang <jinseong.dev@gmail.com>
2022-06-05 16:09:56 +0900

    feat: create project

---
+++ grpc-client/src/test/java/org/debs2/group4/grpcclient/GrpcClientApplicationTests.java
@@ -0,0 +1,13 @@
+package org.debs2.group4.grpcclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ grpc-server/src/test/java/org/debs2/group4/grpcserver/GrpcServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.debs2.group4.grpcserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
